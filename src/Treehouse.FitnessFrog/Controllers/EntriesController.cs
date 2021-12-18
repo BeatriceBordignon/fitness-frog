de06293f7068e0c9e4955c9212324f0900c75518
Beatrice Bordignon <beatrice.bordignon@outlook.com>
2021-12-18 15:05:15 +0100

    Added validation rules using ModelState

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -53,6 +53,13 @@ namespace Treehouse.FitnessFrog.Controllers
         [HttpPost]
         public ActionResult Add(Entry entry)
         {
+
+            if(ModelState.IsValidField("Duration") && entry.Duration<=0)
+            {
+                ModelState.AddModelError("Duration", "The duraton field value must be greater than '0'.");
+            }
+
+
             if (ModelState.IsValid)
             {
                 _entriesRepository.AddEntry(entry);
